8d4f7ad98a5ee6d7f9e8467545a5a239f4bb8103
MD. AHANAF MOHOSEN <mohosen537@gmail.com>
2025-04-17 23:33:07 +0600

    updated

---
+++ kudvenkat/reading & writing to a console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        int name = Console.Read();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
